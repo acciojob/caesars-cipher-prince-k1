33fe04f588db287827c0f87008d9f3179c32551b
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-08-22 00:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -30,12 +30,22 @@ const lookup = {
   "?": "?",
   ",": ",",
 };
-
+	let A = "A".charCodeAt(0) ;
+	let Z = "Z".charCodeAt(0) ;
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return; //return decodedArr
+	let ans = "" ;
+	for(int i = 0; i < encodedStr.length; i++){
+		let curr = str[i].charCodeAt(0);
+		if(curr <= z && curr >= A){
+			ans += lookup[encodedStr[i]];
+		}
+		else{
+			ans += str[i];
+		}
+	}
+  return ans; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
